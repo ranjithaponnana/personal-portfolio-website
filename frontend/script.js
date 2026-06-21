9cca5c08a62d479cd2e10a55c9cce0d0174969f3
ranjithaponnana <ranjithaponnana2007@gmail.com>
2026-06-21 12:27:24 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,54 @@
-console.log("Ranjitha Portfolio Website");
+document
+.getElementById("contactForm")
+.addEventListener("submit", function(event){
+
+
+event.preventDefault();
+
+
+let data = {
+
+name:
+document.getElementById("name").value,
+
+
+email:
+document.getElementById("email").value,
+
+
+message:
+document.getElementById("message").value
+
+};
+
+
+
+fetch("http://127.0.0.1:5000/contact",
+{
+
+method:"POST",
+
+headers:
+{
+"Content-Type":"application/json"
+},
+
+
+body:
+JSON.stringify(data)
+
+})
+
+
+.then(response=>response.json())
+
+
+.then(result=>{
+
+document.getElementById("result").innerHTML =
+result.message;
+
+});
+
+
+});
